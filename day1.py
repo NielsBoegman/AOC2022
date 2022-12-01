99bebf759d1d52098784a0769e1b871ccd0312bb
pokemaster69 <n.boegman.nb@gmail.com>
2022-12-01 11:00:03 +0100

    day1

--- day1.py
+++ day1.py
@@ -1,1 +1,12 @@
-print("fuck me")
+import sys
+currentelf = 0
+elves = []
+for line in sys.stdin:
+    if not line == "\n":
+        currentelf += int(line)
+    else:
+        elves.append(currentelf)
+        currentelf=0
+elves.sort()
+
+print("\n",elves[-1] + elves[-2] + elves[-3])

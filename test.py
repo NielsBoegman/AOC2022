304c6325564a16fecc63da83d5dba3a104da7360
Niels Boegman <n.boegman.nb@gmail.com>
2022-12-07 09:40:53 +0100

    ee

--- test.py
+++ test.py
@@ -1,4 +1,15 @@
-x="succ"
-y="eed"
-z = "succ"
-print(x==y, x==z)
+from dataclasses import dataclass
+from typing import List
+
+@dataclass
+class Test:
+    test: List['Test']
+    name: str
+    parent: 'Test' =""
+
+root = Test([],"a")
+sub = Test([],"b")
+temp = root
+sub.parent=temp
+temp.test.append(sub)
+print(root)

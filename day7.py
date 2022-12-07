48ed61a44c9bf6cf1795aef3576490b90212d6f6
Niels Boegman <n.boegman.nb@gmail.com>
2022-12-07 10:38:17 +0100

    day7

--- day7.py
+++ day7.py
@@ -23,9 +23,34 @@ for line in sys.stdin:
                 for i in range(len(current.dirs)):
                     if current.dirs[i].name == data[2]:
                         current = current.dirs[i]
-            else: current = current.parent
+                        break
+            else: 
+                current.parent.size += current.size
+                current = current.parent
     elif data[0] == "dir":
         current.dirs.append(Dir(data[1],[],0,current))
     else:
         current.size += int(data[0])
-print(root)
+while not current.name == '/':
+    current.parent.size += current.size
+    current = current.parent
+
+def part1(currentdir, result1):
+    if currentdir.size <= 100000:
+        result1 += currentdir.size
+    for directories in currentdir.dirs:
+        result1 = part1(directories, result1)
+    return result1
+
+free = 70000000 - root.size
+reqspace = 30000000 - free
+
+def part2(currentdir, result, required):
+    res = result
+    if currentdir.size >= required and currentdir.size < result:
+        res = currentdir.size
+    for directory in currentdir.dirs:
+        res = part2(directory, res, required)
+    return res
+
+print("Part1: ",part1(root, 0), "Part2: ", part2(root, 999999999, reqspace))

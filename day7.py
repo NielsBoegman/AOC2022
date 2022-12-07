8df151223e5da42fde2dd8a5f94513525c175682
Niels Boegman <n.boegman.nb@gmail.com>
2022-12-07 09:40:52 +0100

    7

---
+++ day7.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass
+from typing import List
+import sys
+
+@dataclass
+class Dir:
+    name: str
+    dirs: List['Dir']
+    size: int
+    parent: 'Dir'
+
+root = Dir('/', [],0,"")
+current = root
+for line in sys.stdin:
+    data = line.split()
+    if line == "$ cd /\n":
+        continue
+    elif data[0] == '$':
+        if data[1] == "ls":
+            continue
+        elif data[1] == "cd":
+            if not data[2] == "..":
+                for i in range(len(current.dirs)):
+                    print(current.dirs[1])
+                    if current.dirs[i].name == data[2]:
+                        current = current.dirs[i]
+            else: current = current.parent
+    elif data[0] == "dir":
+        current.dirs.append(Dir(data[1],[],0,current))
+    else:
+        current.size += int(data[0])
+print(root)
